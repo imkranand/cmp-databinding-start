6d8f4bfc12c44d814f7452ab86177bc596618be6
Kumar Anand <kranand.vnit@gmail.com>
2022-01-23 17:14:08 +0530

    View Encapsulation implementation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+
+  // View Encapsulation None option removes all the other attributes that the Angular adds to the attributes.
+  //  By default the value is set to Emulated as it is browser dependent.
+  // Native option is also there and the native option uses the shadow DOM techonology it gives the same result as Emulated only for the supported browser
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string , content: string};
